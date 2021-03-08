5566cb51c87298589b7a0d11b51e0e1fb9128aaf
Barbosa <barbov1@medtronic.com>
2021-03-08 16:47:50 -0300

    Create lista1_exercicio09.js
    exercicio resolvido 09 (commitado antes do 08 )

---
+++ funcoesJS/listaExercicios/lista1_exercicio09.js
@@ -0,0 +1,24 @@
+
+
+
+
+function roundGrades(pont) {
+    if (pont < 38) {
+        return `${pont} Reprovado `
+    }
+    for (let x = 1; x < 4; ++x) {
+        let res = pont + x;
+        if (res % 5 == 0) {
+            pont = res
+        }
+    }
+    return `${pont} Aprovado`
+
+
+}
+
+console.log(roundGrades(38))  // 40
+console.log(roundGrades(48)) // 50
+console.log(roundGrades(36))// 36
+console.log(roundGrades(20)) // 20
+console.log(roundGrades(47)) // 41
